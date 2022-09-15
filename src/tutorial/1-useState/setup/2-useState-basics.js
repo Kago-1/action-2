d2e144a4d80916bfa7f8bfe0846b07cf98378fd6
Stephen <stephenkago10@gmail.com>
2022-09-15 20:05:36 +0300

    first commit

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'));
+  // const value = useState(0)[1]
+  // const handler = useState(1)[1]
+
+  const [text, setText] = useState('wake up');
+  const eventHandler = () => {
+    if (text === 'wake up') {
+      setText('Sleep');
+    } else {
+      setText('wake up');
+    }
+  };
+  return (
+    <React.Fragment><h1>{text}</h1>,
+      <button className='btn' type='button' onClick={eventHandler}>Change title</button>
+    </React.Fragment>
+    
+  );
 };
 
 export default UseStateBasics;
