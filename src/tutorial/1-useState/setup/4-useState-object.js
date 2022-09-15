39232696436b716d9f24aa940e073a13d138207d
Stephen <stephenkago10@gmail.com>
2022-09-15 23:14:28 +0300

    useState counter

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({ name: 'Peter', age: '26', msg: 'hi' })
+  const changeMesssage = () => {
+
+    setPerson({ ...person, msg: 'hey you' });
+  
+  }
+  
+  return (
+    <div>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.msg}</h2>
+      <button onClick={changeMesssage}>change Messsage</button>
+  </div>
+  )
 };
 
 export default UseStateObject;
