f873086c09157645f2ab0138d8cb7106b3a40fd3
Stephen <stephenkago10@gmail.com>
2022-09-18 14:18:17 +0300

    show/hide

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      <button className='btn' onClick={()=>setShow(!show)}>show/hide</button>
+      {show && <Item/>}
+      
+    </div>
+  )
 };
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = (() => {
+    setSize(window.innerWidth)
+  });
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+    //clean up function cleans up effect from previous render before rerendering
+    //dependency array tells React to skip unnecessarily rerunning of the effect unless there are changes in one of the dependencies since last render
+    return () => { window.removeEventListener('resize', checkSize) };
+  }, [])
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h1>Window</h1>
+      <h2>size :{size}px</h2>
+    </div>
+  )
+}
 
 export default ShowHide;
